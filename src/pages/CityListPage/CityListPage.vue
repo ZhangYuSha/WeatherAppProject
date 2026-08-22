bba05eb913ee17339935964de60ce5590b5b0ef3
ZhangYuSha <isabellaaja21@gmail.com>
2026-08-22 23:04:48 +0800

    feat(router): add CityListPage route and integrate router into app
    - Create CityListPage directory with initial screen component
    - Configure CityListPage route in src/router/index.ts
    - Register router in main.ts
    - Add router-view to App.vue

---
+++ src/pages/CityListPage/CityListPage.vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+    <h1>Weather</h1>
+    <p>City List Page</p>
+  </main>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,17 @@
 //createRouter = creates the router instance
 //createWebHistory = sets up how tohe browser's URL/history is managed
 import { createRouter, createWebHistory } from 'vue-router'
+import CityListPage from '../pages/CityListPage/CityListPage.vue'
 
 const router = createRouter({
     //Ensure the URL look normal rather than using #, require server configuration
     history: createWebHistory(),
     //Empty array for route definitions
-    routes: [],
+    routes: [{
+      path: '/',
+      name: 'city-list',
+      component: CityListPage,
+    },],
 })
 
 export default router
