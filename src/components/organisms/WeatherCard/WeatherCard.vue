d256c588a0cdaef95bbe7c6eee6ee29c0512ddb9
ZhangYuSha <isabellaaja21@gmail.com>
2026-08-23 05:45:33 +0800

    feat(weather-card): set background image based on weather condition icon
    - Map OpenWeatherMap icon codes to local weather background images
    - Add sunny, night, partly-cloudy, rain, thunderstorm, snow, and mist backgrounds
    - Apply matched image as WeatherCard's background via inline style

--- src/components/organisms/WeatherCard/WeatherCard.vue
+++ src/components/organisms/WeatherCard/WeatherCard.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { useRouter } from 'vue-router'
 import './WeatherCard.css'
+
+import mistBackground from '../../../assets/Weather/Day/mist.jpg'
+import nightBackground from '../../../assets/Weather/Day/night.jpg'
+import partlyCloudyBackground from '../../../assets/Weather/Day/partlycloudy.jpg'
+import rainBackground from '../../../assets/Weather/Day/rain.jpg'
+import sunnyBackground from '../../../assets/Weather/Day/sunny.jpg'
+import snowBackground from '../../../assets/Weather/Day/snow.jpeg'
+import thunderstormBackground from '../../../assets/Weather/Day/thunderstorm.jpeg'
 
 const props = defineProps<{
   city: string
@@ -13,6 +22,51 @@ const props = defineProps<{
 }>()
 
 const router = useRouter()
+
+const backgroundImage = computed(() => {
+  switch (props.icon) {
+    case '01d':
+      return sunnyBackground
+
+    case '01n':
+      return nightBackground
+
+    case '02d':
+    case '03d':
+    case '04d':
+      return partlyCloudyBackground
+
+    case '02n':
+    case '03n':
+    case '04n':
+      return nightBackground
+
+    case '09d':
+    case '09n':
+    case '10d':
+    case '10n':
+      return rainBackground
+
+    case '11d':
+    case '11n':
+      return thunderstormBackground
+
+    case '13d':
+    case '13n':
+      return snowBackground
+
+    case '50d':
+    case '50n':
+      return mistBackground
+
+    default:
+      return sunnyBackground
+  }
+})
+
+const cardStyle = computed(() => ({
+  backgroundImage: `url(${backgroundImage.value})`,
+}))
 
 function openWeatherDetail() {
   router.push({
@@ -27,6 +81,7 @@ function openWeatherDetail() {
 <template>
   <article
     class="weather-card"
+    :style="cardStyle"
     tabindex="0"
     role="button"
     :aria-label="`View weather details for ${city}`"
