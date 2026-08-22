a98ee850444157e7407de8d11e5248ad14379b33
ZhangYuSha <isabellaaja21@gmail.com>
2026-08-22 22:59:04 +0800

    feat(router): install and configure Vue Router
    - Install vue-router v5 package
    - Set up router configuration in src/router/index.ts

---
+++ src/router/index.ts
@@ -0,0 +1,12 @@
+//createRouter = creates the router instance
+//createWebHistory = sets up how tohe browser's URL/history is managed
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+    //Ensure the URL look normal rather than using #, require server configuration
+    history: createWebHistory(),
+    //Empty array for route definitions
+    routes: [],
+})
+
+export default router
